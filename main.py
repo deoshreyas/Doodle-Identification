42450bfb141ecda0189cdf630b5ca7543b32b3c4
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-02 15:59:07 +0530

    Finish project, Add prediction capabilities for 4 objects, Add Asynchronous Promises for handling requests

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
 import base64
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
+@app.route('/drawing.png')
+def serve_drawing():
+    return send_from_directory('.', 'drawing.png')
+
 @app.route('/upload-drawing', methods=['POST'])
 def upload_drawing():
-    global counter
-    counter = counter + 1
     data = request.json['image']
     image_data = data.split(",")[1]
     with open("drawing.png", "wb") as fh:
         fh.write(base64.b64decode(image_data))
-    return jsonify({'status': 'success'})
+    # Emit a message to the frontend
+    socketio.emit('image_saved', {'status': 'success'})
+    return jsonify({'status': 'success'})  # Acknowledge the image was processed
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
